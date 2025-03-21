81f636cfceab1be7cb2c5c071de27cb9243e8b50
Farhan <54494073+NajeebFarhan@users.noreply.github.com>
2025-03-21 13:40:10 +0530

    done shit

--- profile.js
+++ profile.js
@@ -1,1 +1,22 @@
-import { } from "./index.html"
+import { collection, addDoc } from "firebase/firestore/lite"; 
+import { db } from "./index.html"
+
+
+const profile_form = document.getElementById("profile")
+
+profile_form.addEventListener("submit", async (e) => {
+
+    e.preventDefault()
+    console.log("success")
+    try {
+    // const docRef = await addDoc(collection(db, "users"), {
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815
+    // });
+    // console.log("Document written with ID: ", docRef.id);
+        window.location.href = "/login.html"
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
+})
