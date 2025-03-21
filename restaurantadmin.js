85bcb42d1773331c423d9a883fa825943402e434
Farhan <54494073+NajeebFarhan@users.noreply.github.com>
2025-03-22 00:40:53 +0530

    test

--- restaurantadmin.js
+++ restaurantadmin.js
@@ -1,3 +1,6 @@
+import { db } from "./firebaaseapp.js"
+import { doc, setDoc, getDoc } from "firebase/firestore/lite"
+
 const donationForm = document.getElementById("donatefood");
 const foodInputsContainer = document.getElementById("foodInputs");
 const addFoodButton = document.getElementById("addFood");
@@ -8,11 +11,13 @@ addFoodButton.addEventListener("click", () => {
     foodItemInput.type = "text";
     foodItemInput.placeholder = "Enter Another Food Item";
     foodItemInput.required = true;
+    foodItemInput.setAttribute("class", "fooditem")
 
     const quantityInput = document.createElement("input");
     quantityInput.type = "number";
     quantityInput.placeholder = "Quantity";
     quantityInput.required = true;
+    quantityInput.setAttribute("class", "quantity")
 
     foodInputsContainer.appendChild(document.createElement("br"));
     foodInputsContainer.appendChild(foodItemInput);
@@ -22,6 +27,34 @@ addFoodButton.addEventListener("click", () => {
 // Handling Form Submission
 donationForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const foodItems = document.getElementsByClassName("fooditem")
+    const quantity = document.getElementsByClassName("quantity")
+    const pincode = document.getElementById("pincode")
+    const location = document.getElementById("location")
+
+    const donations = []
+    for(let i = 0; i < foodItems.length; i++)
+    {
+        donations.push({foodItems: foodItems[i].value, quantity: quantity[i].value, pincode, location})
+    }
+    
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    const docRef = doc(db, "users", user.uid)
+
+    let allDonations = [];
+    async function getData()
+    {
+        const docSnap = await getDoc(docRef)
+        const data = docSnap.data()
+        return data.donations || []
+    }
+    getData().then(val => {
+        allDonations = val;
+    })
+
+    setDoc(docRef, {donations: [...allDonations, ...donations]}, {merge: true})
 
     alert("Thanks for donating!");
 });
