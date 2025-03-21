99d96e3b5ad0aef3b150bec8ec435e0ed8e589f9
Farhan <54494073+NajeebFarhan@users.noreply.github.com>
2025-03-22 02:35:39 +0530

    tt

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ import { getDocs, query, where, collection} from "firebase/firestore/lite"
 
 async function getAllData()
 {
-    const mealsq = query(collection(db, "users"), where("user_type", "==", "restaurant"));
-    const mealsSnapshot = await getDocs(mealsq);
-    // mealsSnapshot.reduce(() => ,)
+    const restaurantq = query(collection(db, "users"), where("user_type", "==", "restaurant"));
+    const restaurantSnapshot = await getDocs(restaurantq)
+    const restaurantSize = restaurantSnapshot.size
+
+    let mealsDonated = 0
+
+    restaurantSnapshot.forEach(doc =>
+    {
+        const data = doc.data()
+        const donation = data.donations?.length || 0
+        mealsDonated += donation
+    })
+
+    const ngoq = query(collection(db, "users"), where("user_type", "==", "ngo"));
+    const ngoSnapshot = await getDocs(ngoq)
+    const ngosize = ngoSnapshot.size
+
+    document.getElementById("meals").innerText = `${mealsDonated}`
+    document.getElementById("donor").innerText = `${restaurantSize}`
+    document.getElementById("ngos").innerText = `${ngosize}`
 }
+
+getAllData()
