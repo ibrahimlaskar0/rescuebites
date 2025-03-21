578d54847e04dea0dd311644ba4593dde3d1dc81
Farhan <54494073+NajeebFarhan@users.noreply.github.com>
2025-03-21 14:42:07 +0530

    db done

--- register.js
+++ register.js
@@ -1,5 +1,6 @@
-import { auth } from "./firebaaseapp.js"
+import { auth, db } from "./firebaaseapp.js"
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore/lite";
 
 const register_form = document.getElementById("register")
 
@@ -8,6 +9,10 @@ register_form.addEventListener("submit", event => {
 
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
+    let name = document.getElementById("name").value
+    let phone = document.getElementById("phone").value
+    let country = document.getElementById("country").value
+    let location = document.getElementById("location").value
 
     console.log("Email:", email);
     console.log("Password:", password);
@@ -18,10 +23,15 @@ register_form.addEventListener("submit", event => {
         // Signed up 
         const user = userCredential.user;
         
-        // await setDoc(doc(db, "user", ))
         console.log(user)
+        await setDoc(doc(db, "user", user.uid), {
+            name,
+            phone,
+            country,
+            location
+        })
 
-        window.location.href = "/login.html"
+        // window.location.href = "/login.html"
 
         })
         .catch((error) => {
